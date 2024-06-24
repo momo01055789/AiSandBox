39e0ae683a903402f18a0036ea5ff111c5fba545
momo01055789 <mohamed.elhadidy666@gmail.com>
2024-06-24 22:24:29 +0300

    testing

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtected = createRouteMatcher(["/", "/profile(.*)"]);
+// const isProtected = createRouteMatcher(["/", "/profile(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtected(req)) auth().protect();
-});
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtected(req)) auth().protect();
+// });
+
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
