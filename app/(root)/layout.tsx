eee342fd80ac43862467e8c7a260d5cff6542163
momo01055789 <mohamed.elhadidy666@gmail.com>
2024-06-24 17:21:03 +0300

    initail commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import MobilelNav from "@/components/shared/MobilelNav";
+import Sidebar from "@/components/shared/Sidebar";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <Sidebar />
+      <MobilelNav />
+      <div className="root_container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
